6cbdb34455e9857c98803ec2ea33e9bda3f4d2ab
Balasubramanian <msbalasubramanian7871006105@gmail.com>
2022-05-09 19:35:43 +0530

    Form Submission Added 🦄💌

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,38 +1,59 @@
+import React, { useRef } from 'react';
 import ContactStyle from "../Styles/Contact.styles";
 import selfie from '../images/selfie.svg';
-const Contact = () => (
-  <ContactStyle id="con">
-    <div>
-      <span>Contact us</span>
-      <p>Contact With Us</p>
-    </div>
-    <form>
-      <div className="name">
-        <label className="red">Name</label>
-        <input type="text" name="user_name" required/>
+
+import emailjs from 'emailjs-com';
+const Contact = () => {
+  const form = useRef();
+
+  const sendMail = e => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_hn72frn', 'template_yz6447y', e.target, 'usDQb92wxOFZV4bh-')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset();
+    };
+  
+
+
+  return (
+    <ContactStyle id="con">
+      <div>
+        <span>Contact us</span>
+        <p>Contact With Us</p>
       </div>
-      <div className="email">
-        <label>Email</label>
-        <input type="email" name="user_email" required />
-      </div>
-      <div className="location">
-        <label>Location</label>
-        <input type="text" name="user_location" required />
-      </div>
-      <div className="subject">
-        <label>Subject</label>
-        <input type="text" name="user_subject" required />
-      </div>
-      <div className="message">
-        <label>Message</label>
-        <textarea name="message" required />
-      </div>
+      <form onSubmit={sendMail}>
+        <div className="name">
+          <label className="red">Name</label>
+          <input type="text" name="user_name" required />
+        </div>
+        <div className="email">
+          <label>Email</label>
+          <input type="email" name="user_email" required />
+        </div>
+        <div className="location">
+          <label>Location</label>
+          <input type="text" name="user_location" required />
+        </div>
+        <div className="subject">
+          <label>Subject</label>
+          <input type="text" name="user_subject" required />
+        </div>
+        <div className="message">
+          <label>Message</label>
+          <textarea name="message" required />
+        </div>
       
-      <input type="submit" className="submit" value="Send Message" />
+        <input type="submit" className="submit" value="Send Message" />
       
-    </form>
-    <img src={selfie} alt="" />
-  </ContactStyle>
-);
+      </form>
+      <img src={selfie} alt="" />
+    </ContactStyle>
+  )
+};
 
 export default Contact;
